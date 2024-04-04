550d541b61792eced260b9e4b3dbf63345313ebe
nickhill28 <snickpick7@gmail.com>
2024-04-05 01:50:23 +0530

    second commit

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware!');
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>')
     next();
 });
 
-app.use((req,res,next)=>{
-    console.log('In another middleware!');
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
     res.send('<h1>Hello Folks!</h1>');
 });
 
