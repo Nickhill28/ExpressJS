d6defc60912184b7a19748b42b23763056a2c93e
nickhill28 <snickpick7@gmail.com>
2024-04-05 03:01:37 +0530

    third commit

--- app.js
+++ app.js
@@ -2,22 +2,18 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>')
-    next();
-});
+app.use('/admin',adminRoute);
+app.use('/shop',shopRoute);
 
-app.post('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Hello Folks!</h1>');
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>404 Error Dont know what youre Looking for...</h1>');
 });
 
 const server = http.createServer(app);
